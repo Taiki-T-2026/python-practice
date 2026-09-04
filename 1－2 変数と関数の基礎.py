3d61df789431e54a71c340f59e602c3f3d505617
Taiki-T-2026 <taketaiken5022@gmail.com>
2026-09-04 19:03:58 +0900

    関数定義・ローカル変数・グローバル変数に関する練習問題を追加

--- 1－2 変数と関数の基礎.py
+++ 1－2 変数と関数の基礎.py
@@ -31,3 +31,38 @@ def seiseki(kokugo,sansu,eigo):
     return heikin
 
 print(seiseki(80,68,89))
+#ローカル変数　関数の中で変数を作った場合、その変数はその関数のみで適応される
+#関数の外でgoukeiやheikinを使っても、外で定義していない限り未定義のエラーが出る。
+#この関数の中で使う、その関数の中だけで適応される変数をローカル変数という
+
+def add(x,y): #引数x,yを足す関数add
+    return x+y
+print(add(6,7))
+
+def rectangle_area(height,width): #引数高さと幅で長方形の面積を出す関数
+    return height*width
+print('長方形の面積は',rectangle_area(6,9))
+
+def greet(name): #名前を入れると「○○さんこんにちわ」と返す関数
+    print(name,'さんこんにちわ')
+
+print(greet('田中'))
+
+def double_sum(x,y):
+    total=x+y
+    return total*2
+print(double_sum(5,6))
+
+tax_rate=0.1
+
+def add_tax(price): #引数「価格」を入れたら10％の税込み価格に変換する関数
+    return price+(price*tax_rate)
+print(add_tax(300))
+
+grand_total=0
+
+def add_total(price,quantity):
+    subtotal=price*quantity
+    return subtotal
+print(add_total(800,9))
+
